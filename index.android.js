45b1eec02880d05af57764caa474b6ab4d440a73
Thiago-Cardoso <trovador182@gmail.com>
2019-08-12 00:00:02 -0300

    test of structure

--- index.android.js
+++ index.android.js
@@ -1,53 +1,47 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
+  Button,
   View
 } from 'react-native';
 
-export default class jokenpo extends Component {
-  render() {
+class MyComponent extends Component{
+
+    render(){
+      return (
+        <View>
+          <Text>{this.props.test}</Text>
+        </View>
+      );
+    }
+}
+
+class jokenpo extends Component{
+
+  constructor(props){
+    super(props);
+
+    this.state = {text : 'Text test 2'};
+  }
+
+  updateText(){
+    this.setState( {text : 'other'} );
+  }
+
+  render(){
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+      <View>
+        <MyComponent test={this.state.text}></MyComponent>
+        <Button 
+          title='Button'
+          onPress={() => {this.updateText()}}
+        />  
       </View>
     );
   }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+}
 
 AppRegistry.registerComponent('jokenpo', () => jokenpo);
