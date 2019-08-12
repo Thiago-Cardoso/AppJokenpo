559c62dadc71923571e9c3bfaa584de3d35982e1
Thiago-Cardoso <trovador182@gmail.com>
2019-08-12 00:46:24 -0300

    development of logic for game

--- index.android.js
+++ index.android.js
@@ -1,43 +1,95 @@
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  Button,
-  View
-} from 'react-native';
+import React, {Component } from 'react'
+import { Text, AppRegistry, Button,  View } from 'react-native';
 
-class MyComponent extends Component{
-
-    render(){
-      return (
-        <View>
-          <Text>{this.props.test}</Text>
-        </View>
-      );
-    }
-}
 
 class jokenpo extends Component{
 
   constructor(props){
     super(props);
 
-    this.state = {text : 'Text test 2'};
+    this.state = { choiceUser : '', 
+                   choiceComputer : '',
+                    result : '' 
+                 }
   }
 
-  updateText(){
-    this.setState( {text : 'other'} );
+  jokenpo(choiceUser){
+
+    //generate randon number
+    var numRandon = Math.floor(Math.random() * 3);
+
+
+    var choiceComputer = '';
+
+    switch(numRandon)
+    {
+      case 0: choiceComputer = 'stone';
+        break;
+      case 1: choiceComputer = 'paper';
+        break;
+      case 2: choiceComputer = 'scissors';
+        break;
+    }
+
+        var result = '';
+
+        if(choiceComputer == 'stone'){
+            if(choiceUser == 'stone'){
+                result = 'draw';
+            }
+
+            if(choiceUser == 'paper'){
+              result = 'User Won';
+           }
+
+           if(choiceUser == 'scissors'){
+            result = 'Computer Won';
+          } 
+        }
+
+          if(choiceComputer == 'paper'){
+            if(choiceUser == 'paper'){
+                result = 'draw';
+            }
+
+            if(choiceUser == 'scissors'){
+              result = 'User Won';
+          }
+
+          if(choiceUser == 'stone'){
+            result = 'Computer Won';
+          } 
+        }
+
+        if(choiceComputer == 'scissors'){
+          if(choiceUser == 'scissors'){
+              result = 'draw';
+          }
+
+          if(choiceUser == 'stone'){
+            result = 'User Won';
+         }
+
+         if(choiceUser == 'stone'){
+          result = 'Computer Won';
+        } 
+       }
+
+    this.setState( { choiceUser : choiceUser,
+                     choiceComputer : choiceComputer,
+                     result: result
+                } );
   }
 
   render(){
     return (
       <View>
-        <MyComponent test={this.state.text}></MyComponent>
-        <Button 
-          title='Button'
-          onPress={() => {this.updateText()}}
-        />  
+         <Text>Choice of Computer: {this.state.choiceComputer}</Text>
+         <Text>Choice of User: {this.state.choiceUser}</Text>
+         <Text>Result: {this.state.result} </Text>
+         <Button title="stone" onPress={ () => { this.jokenpo('stone') }} />
+         <Button title="paper" onPress={ () => { this.jokenpo('paper') }} />
+         <Button title="scissors" onPress={ () => { this.jokenpo('scissors') }} />
       </View>
     );
   }
